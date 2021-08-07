c80fe7976e1be791199fd33c32f8059ff80a53e5
LuisFelipeCruz <luisfelipecruzgeo@hotmail.com>
2021-08-07 10:47:29 -0500

    Initial commit

---
+++ src/main/java/com/example/tiendaminticg4/TiendaMinTicG4Application.java
@@ -0,0 +1,13 @@
+package com.example.tiendaminticg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMinTicG4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaMinTicG4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tiendaminticg4/TiendaMinTicG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendaminticg4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMinTicG4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
